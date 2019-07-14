578480ca14addefda4534302a8f792e1f71bfac1
Mike Eling <mike.eling97@gmail.com>
2019-07-15 00:17:22 +0200

    :rocket: Custom basename for production

--- src/app/App.js
+++ src/app/App.js
@@ -42,7 +42,7 @@ function App() {
           drawerOpen={drawerOpen}
           goTo={goTo}
         />
-        <Router basename="/">
+        <Router basename={process.env.REACT_APP_MODE == "prod" ? "/physiomedica-website" : "/"}>
           <Switch>
             <Route path="/" exact component={Home} />
           </Switch>
