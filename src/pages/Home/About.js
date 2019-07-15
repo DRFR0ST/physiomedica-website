5d66a5f1c6210022d7669d418c041483924dbdfe
Mike Eling <mike.eling97@gmail.com>
2019-07-16 01:06:46 +0200

    :bento: Add about

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/styles'
 
-const About = () => {
-  return <div id="about" />
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    padding: '2vh 30vw',
+  },
+  paragraph: {
+    opacity: 0.45,
+  },
+})
+
+const About = ({ classes }) => {
+  return (
+    <div id="about" className={classes.root}>
+      <Typography variant="h3">About us</Typography>
+      <Typography paragraph className={classes.paragraph}>
+        Consectetur ullamco deserunt velit do officia laboris ipsum enim. Irure
+        est pariatur minim enim deserunt Lorem voluptate tempor minim ad esse
+        cupidatat ad mollit. Enim eu nisi ex velit. Non cillum elit id excepteur
+        mollit mollit sit veniam. In Lorem deserunt sit amet dolor reprehenderit
+        dolor. Exercitation ea aute do dolore non deserunt voluptate.
+      </Typography>
+      <Typography paragraph className={classes.paragraph}>
+        Do aute officia officia velit veniam. Dolor enim veniam anim sunt dolore
+        ex elit incididunt cupidatat nostrud. Minim enim labore excepteur Lorem.
+        Culpa commodo sit est dolor pariatur consequat qui adipisicing cupidatat
+        nulla incididunt reprehenderit ex culpa.
+      </Typography>
+    </div>
+  )
 }
 
-export default About
+export default withStyles(styles)(About)
