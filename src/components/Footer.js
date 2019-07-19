da77e73db2dabbcebeff050a5db7bc1f38f6ba88
Mike Eling <mike.eling97@gmail.com>
2019-07-19 19:32:41 +0200

    :bento: Change Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,11 @@ const styles = theme => ({
     justifyContent: 'flex-end',
     alignItems: 'center',
     padding: '10px 0',
-    background: '#212121',
-    color: '#fff',
+    background: '#f4f4f4',
+    color: '#5a5a5a',
     position: 'relative',
+    'box-shadow':
+      'inset 0 1px 3px rgba(0,0,0,0.12), inset 0 1px 2px rgba(0,0,0,0.24)',
   },
   copyright: {
     display: 'flex',
@@ -31,20 +33,19 @@ const styles = theme => ({
     alignItems: 'center',
     width: '90%',
     padding: '20px 5%',
-    "& p": {
-      marginLeft: "20px"
-    }
+    '& p': {
+      marginLeft: '20px',
+    },
   },
   logo: {
     maxHeight: '200px',
-    marginRight: '10px',
   },
   '@media (max-width: 768px)': {
     infoContainer: {
       flexDirection: 'column-reverse',
-      "& p": {
-        marginLeft: "0px"
-      }
+      '& p': {
+        marginLeft: '0px',
+      },
     },
     logo: {
       marginRight: 0,
@@ -57,11 +58,23 @@ const Footer = ({ classes }) => {
     <div className={classes.root}>
       <div className={classes.infoContainer}>
         <img alt="logo" src={logo} className={classes.logo} />
-        <p>
-          Do aute eiusmod esse ipsum culpa officia laborum cillum in culpa ut eu
-          ex voluptate.
-        </p>
-        <p>In consectetur est pariatur labore aliqua irure enim.</p>
+        <div>
+          <ul>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>ZnanyLekarz</li>
+            <li>YouTube</li>
+          </ul>
+        </div>
+        <div style={{ marginRight: '5vw' }}>
+          <ul>
+            <li>Polityka Prywatności</li>
+            <li>Regulamin</li>
+            <li>Cookies</li>
+            <li>Pomoc</li>
+          </ul>
+        </div>
       </div>
       <div className={classes.copyright}>
         <Typography paragraph>Copyright (c) 2019, PhysioMedica</Typography>
@@ -70,8 +83,20 @@ const Footer = ({ classes }) => {
           <span role="img" aria-label="Heart">
             💖
           </span>{' '}
-          by <a href="https://mike-eling.dev/">ME</a> &{' '}
-          <a href="https://instagram.com/karuutek/">KF</a>
+          by{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://mike-eling.dev/">
+            ME
+          </a>{' '}
+          &{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://instagram.com/karuutek/">
+            KF
+          </a>
         </Typography>
       </div>
     </div>
