01a720572598532aa5692d1fd1eb4c236ea27a90
Mike Eling <mike.eling97@gmail.com>
2019-07-26 00:42:59 +0200

    :wrench: Fix warnings

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -6,7 +6,6 @@ import { withRouter } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
 import CloseIcon from '@material-ui/icons/Close'
 import Slide from '@material-ui/core/Slide'
 
