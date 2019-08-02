87147252778fdee64bb8b9feddcfa6be2a0d97d2
Mike Eling <mike.eling97@gmail.com>
2019-08-02 19:33:14 +0200

    :wrench: Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css'
 import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router basename={process.env.REACT_APP_BASENAME || '/'}>
+  <HashRouter basename={process.env.REACT_APP_BASENAME || '/'}>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 )
 
