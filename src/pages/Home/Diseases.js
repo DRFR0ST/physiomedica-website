b916ec8688927b62c73273c84b80993edba4cf72
Mike Eling <mike.eling97@gmail.com>
2019-08-06 03:53:26 +0200

    :sparkles: Add Diseases

--- src/pages/Home/Diseases.js
+++ src/pages/Home/Diseases.js
@@ -1,33 +1,78 @@
 import React from 'react'
-import { makeStyles, Typography, Divider } from '@material-ui/core'
+import {
+  makeStyles,
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Chip,
+} from '@material-ui/core'
 import { useLittera } from 'react-littera'
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons'
+import illustration from 'images/undraw_Choose_bwbs.svg'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
     padding: '4% 5%',
     margin: '10% 0',
-    justifyContent: 'center',
-    alignItems: 'center',
     background: theme.palette.background.dark,
   },
   items: {
+    maxWidth: '55%',
+    textAlign: 'left',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  chip: {
+    margin: theme.spacing(1),
+    height: 'initial',
+    minHeight: '32px',
+    padding: 5,
+    borderRadius: '200px',
+    '& span': {
+      'white-space': 'normal',
+    },
+  },
+  chipList: {
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
     flexWrap: 'wrap',
   },
-  item: {
-    boxShadow: theme.shadows[1],
-    padding: '10px 15px',
-    borderRadius: '10px',
-    background: theme.palette.background.light,
-    transition: 'box-shadow 255ms ease',
-    margin: '1rem',
-    cursor: 'pointer',
-    userSelect: 'none',
-    '&:hover': {
-      boxShadow: theme.shadows[3],
+  lower: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    position: 'relative',
+  },
+  expPanel: {},
+  illustration: {
+    height: 'auto',
+    width: '100%',
+    maxWidth: 'calc(80% - 4rem)',
+    padding: '0 2rem',
+    position: 'relative',
+
+    '& img': {
+      maxWidth: '100%',
+      maxHeight: '450px',
+    },
+  },
+  '@media (max-width: 768px)': {
+    lower: {
+      flexDirection: 'column',
+    },
+    items: {
+      maxWidth: '100%',
+    },
+    illustration: {
+      display: 'none',
     },
   },
 }))
@@ -42,25 +87,122 @@ const translations = preset => ({
 
 const Diseases = () => {
   const classes = useStyles()
-  const [translated] = useLittera(translations)
+  const [translated, language] = useLittera(translations)
 
-  const items = [
-    {
-      name: 'Schorzenie nr. 1',
-    },
-    {
-      name: 'Schorzenie nr. 2',
-    },
-    {
-      name: 'Schorzenie nr. 3',
-    },
-    {
-      name: 'Schorzenie nr. 4',
-    },
-    {
-      name: 'Schorzenie nr. 5',
-    },
-  ]
+  const items =
+    language === 'pl_PL'
+      ? [
+          {
+            title: 'Fizjoterapia ortopedyczna',
+            elements: [
+              'bole kręgosłupa różnego pochodzenia',
+              'stany po urazach (złamania, skręcenia, zwichnięcia)',
+              'bole barków (m.in bark zamrożony), bóle łokci (łokieć tenisisty, golfisty), bóle nadgarstków, kolan, bioder, stawów skokowych',
+              'stany po endoprotezoplastyce stawów kolanowych, biodrowych',
+              'stany po rekonstrukcji więzadeł ACL, PCL stawu kolanowego',
+              'bole, naciągnięcia, naderwania mięśni',
+              'przeciążenie, przetrenowanie całego organizmu',
+              'zmiany zwyrodnieniowe stawów',
+            ],
+          },
+          {
+            title: 'Fizjoterapia neurologiczna',
+            elements: [
+              'choroby neurologiczne (SM, ch. Parkinsona, polineuropatie, ataksja móżdżkowo-rdzeniowa i inne)',
+              'stany po udarach mózgu',
+              'stany po urazach mózgowo-rdzeniowych',
+            ],
+          },
+          {
+            title: 'Fizjoterapia Uroginekologiczna',
+            elements: [
+              'wysilkowe nietrzymanie moczu',
+              'pęcherz nadreaktywna',
+              'obniżenie narządów miednicy mniejszej',
+              'wypadanie narządów miednicy mniejszej',
+              'bolesne miesiączkowanie',
+              'rozejscie "kresy białej"',
+              'profilaktyka i edukacja',
+            ],
+          },
+          {
+            title: 'Masaż',
+            elements: [
+              'leczniczy, klasyczny',
+              'sportowy',
+              'limfatyczny',
+              'antycellulitowy',
+              'relaksacyjny',
+              'bańką chińską',
+            ],
+          },
+          {
+            title: 'Inne',
+            elements: [
+              'Gimnastyka "zdrowego kręgosłupa"',
+              'Wady postawy, skoliozy',
+            ],
+          },
+        ]
+      : [
+          {
+            title: 'Orthopedic physiotherapy',
+            elements: [
+              'backbone pain of various origins',
+              'conditions after injuries (fractures, sprains, dislocations)',
+              'shoulder pain (including frozen shoulder), elbow pain (tennis elbow, golfer), pain in the wrists, knees, hips, hocks',
+              'conditions after knee and hip arthroplasty',
+              'conditions after reconstruction of ACL ligaments, PCL of the knee joint',
+              'pain, strains, muscle tears',
+              'overloading, overtraining the whole body',
+              'degenerative joint changes',
+            ],
+          },
+          {
+            title: 'Neurological physiotherapy',
+            elements: [
+              "neurological diseases (MS, Parkinson's disease, polyneuropathy, cerebellospinal ataxia and others)",
+              'states after stroke',
+              'states after cerebrospinal injuries',
+            ],
+          },
+          {
+            title: 'Urogynaecological physiotherapy',
+            elements: [
+              'exertional urge incontinence',
+              'overactive bladder',
+              'reduction of pelvic organs',
+              'prolapse of pelvic organs',
+              'painful menstruation',
+              'white border extinction',
+              'prevention and education',
+            ],
+          },
+          {
+            title: 'Massage',
+            elements: [
+              'healing, classic',
+              'sporty',
+              'lymphatic',
+              'Anti-cellulite',
+              'relaxation',
+              'Chinese bubble',
+            ],
+          },
+          {
+            title: 'Other',
+            elements: [
+              'Gymnastics "healthy spine"',
+              'Postural defects, scoliosis',
+            ],
+          },
+        ]
+
+  const [value, setValue] = React.useState(1)
+
+  const handleChange = newValue => {
+    setValue(newValue === value ? -1 : newValue)
+  }
 
   return (
     <div id="diseases" className={classes.root}>
@@ -74,13 +216,39 @@ const Diseases = () => {
         <Typography paragraph style={{ opacity: 0.5 }}>
           {translated.slogan}
         </Typography>
-        <br/>
-        <div className={classes.items}>
-          {[...items, ...items, ...items, ...items].map((e, i) => (
-            <div key={`disease_item_${i}`} className={classes.item}>
-              {e.name}
-            </div>
-          ))}
+        <br />
+        <div className={classes.lower}>
+          <div className={classes.items}>
+            {items.map((item, i) => (
+              <ExpansionPanel
+                expanded={value === i}
+                onChange={() => handleChange(i)}
+                className={classes.expPanel}>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel4bh-content"
+                  id="panel4bh-header">
+                  <Typography className={classes.heading}>
+                    {item.title}
+                  </Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails className={classes.chipList}>
+                  {item.elements
+                    //.sort((a, b) => a.length - b.length)
+                    .map((e, i) => (
+                      <Chip
+                        color="primary"
+                        className={classes.chip}
+                        label={e}
+                      />
+                    ))}
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            ))}
+          </div>
+          <div className={classes.illustration}>
+            <img src={illustration} alt="illustration diseases" />
+          </div>
         </div>
       </div>
     </div>
